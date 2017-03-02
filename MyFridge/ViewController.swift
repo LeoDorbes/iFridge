fdbaeeca7967bedbc2bf30496a9009b771808b00
Alexia Longval <al162646@imac-de-administrateur-6.local>
2017-03-02 09:50:29 +0100

    Initial Commit

---
+++ MyFridge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFridge
+//
+//  Created by Alexia Longval on 02/03/2017.
+//  Copyright © 2017 Alexia Longval. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
