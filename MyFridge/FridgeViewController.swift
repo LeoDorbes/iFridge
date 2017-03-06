8a896d8ad07f5b4507b25930ca0a8f20ada4b31c
Alex Hawk <alexhawk@Alexs-Mac.local>
2017-03-06 16:32:32 +0100

    Added delete button to fridge

--- MyFridge/FridgeViewController.swift
+++ MyFridge/FridgeViewController.swift
@@ -12,7 +12,6 @@ class FridgeViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -53,5 +52,15 @@ class FridgeViewController: UITableViewController {
         cell.textLabel?.text = FridgeItemListSingleton.instance.getElementAt(row: row).getName()
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.delete) {
+            // handle delete (by removing the data from your array and updating the tableview)
+        }
     }
 }
