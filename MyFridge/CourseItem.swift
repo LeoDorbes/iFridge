d67b06a42997a723a526b7b650189f0e723f5eda
PARadiguet <sandracamacho@macbook-pro-de-pierre-andre.local>
2017-03-06 20:00:26 +0100

    Add of Aliment

--- MyFridge/CourseItem.swift
+++ MyFridge/CourseItem.swift
@@ -9,10 +9,11 @@
 class CourseItem{
     
     var  name : String
-
+    var  selected : Bool
 
     public init(n : String) {
         self.name = n
+        self.selected = false
     }
     
     public func getName() -> String{
@@ -21,6 +22,18 @@ class CourseItem{
     
     public func setName(n : String){
         self.name = n
+    }
+    public func switchstate(){
+        if(self.selected == true){
+            self.selected = false
+        }
+        else {
+            self.selected = true
+        }
+        print(self.selected)
+    }
+    public func getState() -> Bool {
+        return self.selected
     }
     
 }
